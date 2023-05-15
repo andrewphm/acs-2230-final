366f6395c7dc42b1ccc5dad5c92db1c3ee2caea8
Andrew Pham <andrew.pham@shopify.com>
2023-05-14 22:14:32 -0400

    Complete Category controller basic CRUD actions

---
+++ controllers/category.js
@@ -0,0 +1,67 @@
+const Category = require('../models/Category');
+
+const createCategory = async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(201).json({ category });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getOneCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await Category.findById(id);
+    if (category) {
+      return res.status(200).json({ category });
+    } else {
+      return res.status(404).json({ message: 'Category not found!' });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    return res.status(200).json({ categories });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Category.findByIdAndDelete(id);
+    if (deleted) {
+      return res.status(200).send('Category deleted');
+    }
+    throw new Error('Category not found');
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const updateCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedCategory = await Category.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updatedCategory) {
+      return res.status(404).json({ message: 'Category not found!' });
+    }
+    res.status(200).json({ category: updatedCategory });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createCategory,
+  getAllCategories,
+  getOneCategory,
+  deleteCategory,
+  updateCategory,
+};
